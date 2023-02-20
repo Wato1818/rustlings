96eeac73eefe61a9b6b378a1465dc2e733b1240d
Gonzalo-77 <gdiaz@biobiochile.cl>
2023-02-20 17:38:39 -0300

    avance

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,8 +2,6 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let mut vec0 = Vec::new();
 
@@ -21,4 +19,5 @@ fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
+    vec.to_vec()
 }
